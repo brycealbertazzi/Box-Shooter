3ecdbcf9ced85f0f421024a58705a3820b5139c4
Bryce Albertazzi <brycealbertazzi@gmail.com>
2025-09-10 18:03:48 -0600

    Update build settings

---
+++ Library/Bee/artifacts/iOS/il2cppOutput/cpp/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	5642,
+	g_Il2CppGenericTypes,
+	3353,
+	g_Il2CppGenericInstTable,
+	47458,
+	g_Il2CppGenericMethodFunctions,
+	16078,
+	g_Il2CppTypeTable,
+	55547,
+	g_Il2CppMethodSpecTable,
+	4740,
+	g_FieldOffsetTable,
+	4740,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
